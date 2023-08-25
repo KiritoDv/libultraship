6abea588cd99ca2d4325fdae2147f521aba4f3e8
KiritoDv <kiritodev01@gmail.com>
2023-08-24 23:11:44 -0600

    Added TASController, fixed rendering issue and added Animation type

--- src/resource/ResourceType.h
+++ src/resource/ResourceType.h
@@ -16,20 +16,8 @@ enum class ResourceType {
     Texture = 0x4F544558,     // OTEX
 
     // LUS of Harkinian
-    SOH_Animation = 0x4F414E4D,       // OANM
-    SOH_PlayerAnimation = 0x4F50414D, // OPAM
-    SOH_Room = 0x4F524F4D,            // OROM
-    SOH_CollisionHeader = 0x4F434F4C, // OCOL
-    SOH_Skeleton = 0x4F534B4C,        // OSKL
-    SOH_SkeletonLimb = 0x4F534C42,    // OSLB
-    SOH_Path = 0x4F505448,            // OPTH
-    SOH_Cutscene = 0x4F435654,        // OCUT
-    SOH_Text = 0x4F545854,            // OTXT
-    SOH_Audio = 0x4F415544,           // OAUD
-    SOH_AudioSample = 0x4F534D50,     // OSMP
-    SOH_AudioSoundFont = 0x4F534654,  // OSFT
-    SOH_AudioSequence = 0x4F534551,   // OSEQ
-    SOH_Background = 0x4F424749,      // OBGI
-    SOH_SceneCommand = 0x4F52434D,    // ORCM
+    Demo = 0x44454D4F,        // DEMO
+    Skybox = 0x534B5942,      // SKYB
+    Anim = 0x414E494D,        // ANIM
 };
 } // namespace LUS
