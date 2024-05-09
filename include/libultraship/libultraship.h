c62fe029942b9c7791e9d7c14cd50d4a41328791
KiritoDv <kiritodev01@gmail.com>
2024-05-08 19:46:56 -0600

    Random fix

--- include/libultraship/libultraship.h
+++ include/libultraship/libultraship.h
@@ -10,4 +10,10 @@
 #include "classes.h"
 #endif
 
+#if defined(_WIN32)
+    #define ALIGN_ASSET(x) __declspec(align(x))
+#else
+    #define ALIGN_ASSET(x) __attribute__((aligned (x)))
+#endif
+
 #endif
